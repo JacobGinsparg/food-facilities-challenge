2a8c93c94823464fde50ef8d08652d34ad09fde7
Jacob Ginsparg <jacobginsparg@gmail.com>
2023-08-03 14:09:31 -0400

    Route and queries for street name search, skeleton for location

--- src/food_facilities_challenge/database.py
+++ src/food_facilities_challenge/database.py
@@ -10,12 +10,24 @@ class Database:
     def __init__(self) -> None:
         self._db = read_csv("Mobile_Food_Facility_Permit.csv")
 
+    # Internal query helpers
+
     def _query(self, field_name: str, value):
         return json.loads(self._db.query(f"{field_name} == '{value}'").to_json(orient="records"))
 
     def _query_multi(self, query_params: Dict[str, str]):
         query_string = " & ".join([f"{field} == '{value}'" for field, value in query_params.items()])
         return json.loads(self._db.query(query_string).to_json(orient="records"))
+
+    def _query_partial_str(self, field_name: str, value):
+        return json.loads(
+            self._db.query(
+                f"{field_name}.str.contains('{value}', regex=False)",
+                engine="python",
+            ).to_json(orient="records")
+        )
+
+    # External query methods
 
     def query_applicant(self, applicant: str, status: str = None):
         if status:
@@ -27,6 +39,15 @@ class Database:
             )
         else:
             return self._query(field_name="Applicant", value=applicant)
+
+    def query_street(self, street: str):
+        return self._query_partial_str(field_name="Address", value=street)
+
+    def query_near(self, lat: float, long: float, allow_non_approved: bool):
+        if allow_non_approved:
+            pass
+        else:
+            pass
 
 
 # Initialize db object
--- src/food_facilities_challenge/routers/facilities.py
+++ src/food_facilities_challenge/routers/facilities.py
@@ -23,3 +23,29 @@ def get_facilities_by_applicant(applicant: str, status: str = None):
             "Facilities": applicant_facilities,
         }
     )
+
+
+@router.get("/search/street/{street}")
+def get_facilities_by_street(street: str):
+    street_facilities = db.query_street(street=street)
+    return JSONResponse(
+        {
+            "Count": len(street_facilities),
+            "Facilities": street_facilities,
+        }
+    )
+
+
+@router.get("/search/location/{lat}/{long}")
+def get_nearby_facilities(lat: float, long: float, allow_non_approved: bool = False):
+    nearby_facilities = db.query_near(
+        lat=lat,
+        long=long,
+        allow_non_approved=allow_non_approved,
+    )
+    return JSONResponse(
+        {
+            "Count": len(nearby_facilities),
+            "Facilities": nearby_facilities,
+        }
+    )
